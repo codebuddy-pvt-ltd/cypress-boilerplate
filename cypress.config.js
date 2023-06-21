b73b71b9f7b32e2388c67fddfe1df8c0cffb9dbb
Sohan Dutta <sohan@codebuddy.co>
2023-06-21 19:52:48 +0530

    :sparkles: feat: add env

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,18 @@
 const { defineConfig } = require("cypress");
+const dotenv = require("dotenv");
+
+dotenv.config();
 
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
+  },
+  env: {
+    APP_URL: process.env.CYPRESS_APP_URL,
+    API_URL: process.env.CYPRESS_API_URL,
+    USERNAME: process.env.CYPRESS_USERNAME,
+    PASSWORD: process.env.CYPRESS_PASSWORD,
   },
 });
--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+describe("Login", () => {
+  beforeEach(() => {
+    cy.request("POST", `${Cypress.env("API_URL")}/auth/login`, {
+      email: Cypress.env("USERNAME"),
+      password: Cypress.env("PASSWORD"),
+      keepMeLoggedIn: true,
+    }).then((res) => {
+      cy.setCookie(
+        "interior-service-admin-auth",
+        res.body.data.tokens.accessToken
+      );
+    });
+  });
+
+  it("Login to Admin", () => {
+    cy.visit(Cypress.env("APP_URL"));
+  });
+
+  it("Login to Admin Products", () => {
+    cy.visit(Cypress.env("APP_URL") + "/masters/products");
+  });
+});
